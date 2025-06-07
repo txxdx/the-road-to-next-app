0f041d485486b423fdfc32b34d5077eaa49c2851
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2025-06-07 21:05:59 +0800

    fix build error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
-  },
 ];
 
 export default eslintConfig;
