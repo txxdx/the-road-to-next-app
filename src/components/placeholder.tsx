54b928160850579e41be6bd532de21047427107c
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2025-06-07 21:31:24 +0800

    fix build error

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -3,7 +3,7 @@ import { cloneElement } from 'react';
 
 type PlaceholderProps = {
 	label: string;
-	icon?: React.ReactElement;
+	icon?: React.ReactElement<React.ComponentPropsWithoutRef<"svg">>;
 	button?: React.ReactElement;
 };
 
@@ -14,7 +14,7 @@ const Placeholder = ({
 }: PlaceholderProps) => {
 	return (
 		<div className="flex-1  self-center flex flex-col items-center justify-center gap-y-2">
-			{cloneElement(icon)}
+			{cloneElement(icon, { className: 'w-4 h-4' })}
 			<h2 className="text-lg text-center">{label}</h2>
 			{cloneElement(button)}
 		</div>
